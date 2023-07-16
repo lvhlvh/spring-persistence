eb9c32d48c94868df45f85f975879a34445cc93e
lvhao <riviewho@gmail.com>
2023-07-16 18:09:33 +0800

    feat: init sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into balance (user_id, user_name, balance)
+values (1, 'Tom', 100),
+       (2, 'Alice', 200);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+use spring_persistence;
+create table if not exists balance
+(
+    `user_id`   int(10) primary key,
+    `user_name` varchar(20) not null,
+    `balance`   int(10)     not null default 0
+    ) engine = InnoDB
+    default character set = utf8mb4;
