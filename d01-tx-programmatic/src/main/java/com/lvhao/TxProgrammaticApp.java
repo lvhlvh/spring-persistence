6e59b7c31dccedf7a4327d28ae11d3b954db486e
lvhao <riviewho@gmail.com>
2023-07-16 18:37:51 +0800

    feat: 新增module 编程式事务管理

---
+++ d01-tx-programmatic/src/main/java/com/lvhao/TxProgrammaticApp.java
@@ -0,0 +1,11 @@
+package com.lvhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxProgrammaticApp {
+  public static void main(String[] args) {
+    SpringApplication.run(TxProgrammaticApp.class, args);
+  }
+}
